adb49e9ba3ad87403436efc482ccc0f885b7bf6f
Bluebender <olliviev@gmail.com>
2023-02-21 16:19:37 +0000

    Fin du PHP

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -11,14 +11,14 @@ class UserController extends AbstractController {
     public function getUsers()
     {
         // get all the users from the manager
-        $users = $this->um->getAllUsers();
-        $usersArray=[];
-        foreach ($users as $user){
+        $allUsers = $this->um->getAllUsers();
+        $allUsersArray=[];
+        foreach ($allUsers as $user){
             $userArray=$user->toArray();
-            $usersArray[]=$userArray;
+            $allUsersArray[]=$userArray;
         }
         // render
-        $this->render($usersArray);
+        $this->render($allUsersArray);
     }
 
     public function getUser(string $get)
@@ -26,31 +26,43 @@ class UserController extends AbstractController {
         // get the user from the manager
         $id=intval($get);
         $user = $this->um->getUserById($id);
+        $userArray=$user->toArray();
         // either by email or by id
 
         // render
-        $this->render($user);
-
+        $this->render($userArray);
     }
 
     public function createUser(array $post)
     {
         // create the user in the manager
+        $UserToCreate=new User (null, $post[1], $post[2], $post[3], $post[4], $post[5]);
+        $userCreated = $this->um->createUser($UserToCreate);
+        $userCreatedArray=$userCreated->toArray();
 
         // render the created user
+        $this->render($userCreatedArray);
     }
 
     public function updateUser(array $post)
     {
         // update the user in the manager
+        $UserToUpdate=new User ($post[0], $post[1], $post[2], $post[3], $post[4], $post[5]);
+        $userUpdated = $this->um->updateUser($UserToUpdate);
+        $userUpdatedArray=$userUpdated->toArray();
 
         // render the updated user
+        $this->render($userUpdatedArray);
     }
 
     public function deleteUser(array $post)
     {
         // delete the user in the manager
+        $UserToDelete=new User ($post[0], $post[1], $post[2], $post[3], $post[4], $post[5]);
+        $NewUserTab = $this->um->updateUser($UserToDelete);
+        $NewUserTabArray=$NewUserTab->toArray();
 
         // render the list of all users
+        $this->render($NewUserTabArray);
     }
 }
